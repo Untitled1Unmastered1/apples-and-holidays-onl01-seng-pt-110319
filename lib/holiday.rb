6399b0446b85cc404cad682ebb540a57d59f8192
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-27 07:00:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,7 +84,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |seasons, holidays|
     holiday_hash[seasons].each do |holidays, supplies|
       holiday_hash[seasons][holidays].each do |supplies|
-        
+        if supplies == "BBQ"
+          
 
 end
 
