263fc9de9623f501b2a6d94a29b6b28272d0f53d
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-27 06:53:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,6 +50,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   winter_supplies = [] 
   holiday_hash[:winter].each  do |holidays, supplies|
+  holiday_hash[:winter][holidays] = [supplies].each do |item|
 
 end
 
