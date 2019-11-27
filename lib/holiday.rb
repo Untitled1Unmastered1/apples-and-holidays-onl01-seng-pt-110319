71525aefcb2b241c612c8f392a05320fe95fa0b2
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-27 06:48:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,13 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  holiday_hash.each do |season , holiday|
-    if season == :spring
-      holiday.each do |occasion , supplies|
-        return "#{supplies.join("")}"
-     end 
-  end 
-end 
+  holiday_hash[:summer][:fourth_of_july ][1]
  
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
